9a334405d005d223a166979bf6b74fb20b9475d5
qicongsheng <875881559@qq.com>
2025-08-06 12:59:51 +0800

    1.0.90.10

--- maven_proxy/db.py
+++ maven_proxy/db.py
@@ -1,9 +1,10 @@
 # 修改 db.py 文件
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author: qicongsheng
 import sqlite3
 import time
+
 
 class DB:
     def __init__(self, database_file_path='./database.db'):
@@ -12,21 +13,13 @@ class DB:
 
     def init_tables(self):
         # 创建错误记录表
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS fetch_errors (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                remote_url TEXT NOT NULL,
-                error_message TEXT,
-                timestamp INTEGER NOT NULL
-            )
-        ''')
+        self.conn.execute(
+            'CREATE TABLE IF NOT EXISTS fetch_errors (id INTEGER PRIMARY KEY AUTOINCREMENT, remote_url TEXT NOT NULL, error_message TEXT, imestamp INTEGER NOT NULL)')
         self.conn.commit()
 
     def record_fetch_error(self, remote_url, error_message):
-        self.conn.execute('''
-            INSERT INTO fetch_errors (remote_url, error_message, timestamp)
-            VALUES (?, ?, ?)
-        ''', (remote_url, error_message, int(time.time())))
+        self.conn.execute('INSERT INTO fetch_errors (remote_url, error_message, timestamp)VALUES (?, ?, ?)',
+                          (remote_url, error_message, int(time.time())))
         self.conn.commit()
 
     def has_fetch_failed_before(self, remote_url):
@@ -36,4 +29,3 @@ class DB:
         cursor = self.conn.execute('SELECT COUNT(*) FROM fetch_errors WHERE remote_url = ?', (remote_url,))
         count = cursor.fetchone()[0]
         return count > 0
-
